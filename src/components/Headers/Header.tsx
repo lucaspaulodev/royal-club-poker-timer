503e6e9dc509ee8d6fe9baf764e14de84b6275b4
Lucas Paulo <lucaspaulodev@gmail.com>
2024-05-12 21:10:27 -0300

    feat: Fixing header layout.

--- src/components/Headers/Header.tsx
+++ src/components/Headers/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
   const { pathname } = useLocation();
 
   return (
-    <header className="flex first-line:px-4 py-4 text-2xl font-semibold bg-zinc-950">
+    <header className="flex first-line:px-4 py-4 px-8 text-2xl font-semibold bg-zinc-950">
       {pathname === '/timer' ? <TimerHeaderContent /> : <HomeHeaderContent />}
     </header>
   );
